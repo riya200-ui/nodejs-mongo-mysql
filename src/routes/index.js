9b39b6c77d7c204b53df4bdd675b5ead4d91a367
riya <panchalriya1976@gmail.com>
2022-05-02 17:00:20 +0530

    modify routes index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,27 @@
 //The express.Router() function is used to create a new router object. This function is used when you want to create a new router object in your program to handle requests.
 const express = require('express');
-const {student, teacher} = require("../controllers/")
+
+const {student} = require("../controllers/student")
+const {teacher}= require("../controllers/teacher")
+//const Student = require("../models/student")
 
 const router = express.Router();
 
-router.get('/',student.get);
-router.post('/',student.create);
+router.get('/', function(req, res){
+    res.json({
+        status: 'API works',
+        message: 'Welcome to Car Rental Agency services'
+    })
+ })
+
+//router.get('/',student.get);
+router.post('/',student.create,()=>{
+    try{
+        res.status(200).json('yes')
+    }catch{
+        res.status(400).json('no')
+    }
+});/*
 router.patch('/',student.update);
 router.patch('/:id',student.update);
 router.delete('/',student.delete);
@@ -16,6 +32,6 @@ router.post('/',teacher.create);
 router.patch('/',teacher.update);
 router.patch('/:id',teacher.update);
 router.delete('/',teacher.delete);
-router.delete('/:id',teacher.delete);
+router.delete('/:id',teacher.delete);*/
 
 module.exports = router
